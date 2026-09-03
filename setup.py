1f51e9201bc90d97aed4f8b42d45811b1395c036
pramod <kumardp73@gmail.com>
2026-09-03 17:53:05 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney_disease"
+AUTHOR_USER_NAME = "Pramod050"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "kumardp73@gmail.com"
